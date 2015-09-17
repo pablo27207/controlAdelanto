f52531f310423a09826ead24149e389a15329650
Pablo <pablo27207@gmail.com>
2015-09-17 16:38:37 -0300

    solucionado lo del driver Mysqli

--- index.php
+++ index.php
@@ -191,6 +191,12 @@ if (defined('ENVIRONMENT'))
 		define('APPPATH', BASEPATH.$application_folder.'/');
 	}
 
+	/* --------------------------------------------------------------------
+	 * LOAD THE DATAMAPPER BOOTSTRAP FILE
+	 * --------------------------------------------------------------------
+	 */
+	require_once APPPATH.'third_party/datamapper/bootstrap.php';
+	
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
@@ -200,12 +206,6 @@ if (defined('ENVIRONMENT'))
  *
  */
 require_once BASEPATH.'core/CodeIgniter.php';
-
-/* --------------------------------------------------------------------
- * LOAD THE DATAMAPPER BOOTSTRAP FILE
- * --------------------------------------------------------------------
- */
-require_once APPPATH.'third_party/datamapper/bootstrap.php';
 
 /* End of file index.php */
 /* Location: ./index.php */
