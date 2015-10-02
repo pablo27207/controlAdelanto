6265cbd1ffd2a354927cffed7823f6bf7a7b6def
Pablo <pablo27207@gmail.com>
2015-10-02 11:42:00 -0300

    agregado funcionamiento de ci_session con bd

--- application/views/transaccionView.php
+++ application/views/transaccionView.php
@@ -1,1 +1,56 @@
-<?php
+ <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper" id="paginaTransaccion">
+          	<h3><i class="fa fa-angle-right"></i> Transacciones</h3>
+				<div class="row mb">
+        
+				   <!-- page start-->
+                  <div class="content-panel">
+                        <div class="adv-table">
+                            <table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered" id="hidden-table-info">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Usuario</th>
+                                    <th class="hidden-phone">Nombre</th>
+                                    <th class="hidden-phone">Apellido</th>
+                                    <th class="hidden-phone">Tel&eacute;fono</th>
+                                     <th class="hidden-phone">Email</th>                      
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach ($data['data_transaccion'] as $row){ ?>
+                    	  <?php if (isset($row-> estado) && (strcmp($row ->estado,'baja')==0)){
+                    	   echo('<tr class="danger">');}
+                        else{
+                            echo '<tr>';}
+                       ?>
+                    	
+                    		<td><?php echo $row -> dni; ?></td>
+                    		<td><?php echo $row -> legajo; ?></td>
+                            <td><?php echo $row -> nombre; ?></td>
+                            <td><?php echo $row -> apellido; ?></td>
+                            <td><?php echo $row -> telefono; ?></td>
+                             <td><?php echo $row -> email; ?></td>
+                             <a href=""><i class="fa fa-times fa-fw"></i></a>
+                            
+                              
+                              	</tr>
+                    	<?php 
+                    }?>
+                                </tbody>
+                            </table>
+                        </div>
+                  </div>
+              <!-- page end-->
+
+				
+              </div><!-- /row -->
+
+		</section> <!--/wrapper -->
+      </section><!-- /MAIN CONTENT -->
+
+      <!--main content end-->
